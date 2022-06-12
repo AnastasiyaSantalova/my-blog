463013306a5e653cd9d9293c8e0619e202d9a295
Nastia <nastia@Air-Iliia.lan>
2022-06-12 17:09:21 +0400

    [001-pages] main website pages added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
 import './App.css';
+
+import AboutPage from './pages/about/AboutPage';
+import ArticlePage from './pages/article/ArticlePage';
+import ArticlesListPage from './pages/articles-list/ArticlesListPage';
+import ErrorPage from './pages/error/ErrorPage';
+import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/articles' element={<ArticlesListPage />} />
+            <Route path='/article' element={<ArticlePage />} />
+            <Route path='/*' element={<ErrorPage />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
---
+++ src/pages/home/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <>
+            <h1>Hello, welcome to my blog!</h1>
+        </>
+    )
+}
+
+export default HomePage;
